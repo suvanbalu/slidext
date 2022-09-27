1dbc70643018f48171e9e08c231f8bc0f3ce5dd7
suvanbalu <suvanbalu@gmail.com>
2022-09-27 12:25:12 +0530

    added slidesconverter

--- pdf_ppt.py
+++ pdf_ppt.py
@@ -1,6 +1,14 @@
 from fpdf import FPDF
 import os
 from PIL import Image
+# Copyright (c) 2020 Kevin McGuinness <kevin.mcguinness@gmail.com>
+import fitz
+import io
+
+from tqdm import trange
+from pptx import Presentation
+from pptx.util import Cm
+from pathlib import Path
 
 def convert_to_pdf(path,dest,name):
     final=[]
@@ -13,6 +21,61 @@ def convert_to_pdf(path,dest,name):
     imgc0=img0.convert("RGB")
     imgc0.save(f"{dest}/{name}.pdf",save_all=True,append_images=final)
 
+def convert_to_ppt(input_path, output_file, resolution=300, start_page=0, page_count=None,quiet=True):
+    convert_to_pdf(input_path,os.getcwd(),"temp")
+    pdf_file = "temp.pdf"
+    doc = fitz.open("temp.pdf")
+    if not quiet:
+        print(pdf_file, 'contains', doc.page_count, 'slides')
+
+    if page_count is None:
+        page_count = doc.page_count
+
+    # transformation matrix: slide to pixmap
+    zoom = resolution / 72
+    matrix = fitz.Matrix(zoom, zoom, 0)
+
+    # create pptx presentation
+    prs = Presentation()
+    blank_slide_layout = prs.slide_layouts[6]
+
+    # configure presentation aspect ratio
+    page = doc.load_page(0)
+    aspect_ratio = page.rect.width / page.rect.height
+    prs.slide_width = int(prs.slide_height * aspect_ratio)
+    
+    # create page iterator
+    if not quiet:
+        page_iter = trange(start_page, start_page + page_count)
+    else:
+        page_iter = range(start_page, start_page + page_count)
+
+    # iterate over slides
+    for page_no in page_iter:
+        page = doc.load_page(page_no)
+
+        # write slide as a pixmap
+        pixmap = page.get_pixmap(matrix=matrix)
+        image_data = pixmap.tobytes(output='PNG')
+        image_file = io.BytesIO(image_data)
+    
+        # add a slide
+        slide = prs.slides.add_slide(blank_slide_layout)    
+        left = top = Cm(0)
+        slide.shapes.add_picture(image_file, left, top, height=prs.slide_height)
+
+    if output_file is None:
+        output_file = Path(pdf_file).with_suffix('.pptx')
+    
+    # save presentation
+    prs.save(output_file)
+    doc.close()
+    os.remove(os.getcwd()+"\\temp.pdf")
+
+convert_to_ppt("test_photos/test6","test.pptx")
 
 
-convert_to_pdf("test_photos/test6","test_pdf","test6")
+# input_path="test_photos/test6"
+# convert_to_pdf(input_path,os.getcwd(),"temp")
+
+# convert_to_pdf("test_photos/test6","test_pdf","test6")
